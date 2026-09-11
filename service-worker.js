4af3f25f1807a9112e5b5deeb207541c420fd046
ruggineconsultoria-cyber <ruggineconsultoria@gmail.com>
2026-09-10 23:25:55 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'emaus-shell-v37';
+const CACHE_NAME = 'emaus-shell-v38';
 const APP_SHELL = ['./', './index.html', './styles.css', './splash.css', './splash.js', './app.js', './api-config.js', './manifest.json', './bethesda-logo.png', './recepcao.html', './reception.js', './admin.html', './admin.css', './admin.js', './emaus-admin-logo.png', './publica.html', './publica.css', './publica.js'];
 
 self.addEventListener('install', event => {
